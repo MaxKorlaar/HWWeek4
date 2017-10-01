26d483acb8bd70c8540b7e2e5e312c629cb293b1
Roonschot <g.manschot@gmail.com>
2017-10-01 19:40:17 +0200

    wow
    wow

---
+++ Practice Exercise 7_2 (while-loop).py
@@ -0,0 +1,12 @@
+def vierletter():
+    while True:
+        woordje = input("Voer een string van vier letters in: ")
+        lengte = len(woordje)
+        if lengte == 4:
+            print("Inleren van correcte string:", woordje, "is geslaagd")
+            break
+        else:
+            print(woordje, "heeft", lengte, "letters")
+            woordje
+
+vierletter()
