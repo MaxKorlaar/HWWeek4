fc8674ebdaa546365bd4d637263e7a0da26854a8
Max Korlaar <max@maxkorlaar.nl>
2018-10-29 18:18:21 +0100

    Update Practice Exercise 7_2 (while-loop).py
    Consistente formatting

--- Practice Exercise 7_2 (while-loop).py
+++ Practice Exercise 7_2 (while-loop).py
@@ -3,7 +3,7 @@ def vierletter():
         woordje = input("Voer een string van vier letters in: ")
         lengte = len(woordje)
         if lengte == 4:
-            print("Inleren van correcte string: '"+ woordje + "' is geslaagd")
+            print("Inleren van correcte string: '" + woordje + "' is geslaagd")
             break
         else:
             print("'" + woordje + "' heeft " + str(lengte) + " letters")
